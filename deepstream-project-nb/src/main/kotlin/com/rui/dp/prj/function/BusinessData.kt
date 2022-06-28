d15e968a660590e4979e6f2c37ac47ae6fc127d6
远夏 <farawaysummer.shu@gmail.com>
2022-06-28 12:29:17 +0800

    remove projects

--- deepstream-project-nb/src/main/kotlin/com/rui/dp/prj/function/BusinessData.kt
+++
@@ -1,9 +0,0 @@
-package com.rui.dp.prj.function
-
-data class BusinessData(
-    val businessName: String,
-    val dsName: String,
-    val businessSql: String,
-    val conditionFields: List<String>,
-    val resultFields: Map<String, String>
-)
